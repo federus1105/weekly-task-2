33dc57e7c6c078a40dcf313a3a665780c44a8d25
federus rudi <federusrudi@gmail.com>
2025-07-14 10:18:21 +0700

    feat: menambahkan file task-4.js, fiturnya memisah bilangan integer berdasarkan 0, setelah di pisah di urutkan secara ascending

--- tugas/task-4.js
+++ tugas/task-4.js
@@ -1,24 +1,27 @@
-export function devideAndSort (num) {
-    // mengubah angka menjadi string
-    let numStr = num.toString()
+export function devideAndSort(num) {
+  // mengubah angka menjadi string
+  let numStr = num.toString();
 
-    // memisah string angka berdasarkan 0 dan menjadi array
-    let numPisah = numStr.split("0")
+  // memisah string angka berdasarkan 0 dan menjadi array
+  let numPisah = numStr.split("0");
 
-    // memecah string angka menjadi array karakter / digit-digit
-    let urut = numPisah.map(pisah =>pisah.split(``)
-    // map memproses numPisah dan menjadi array baru
+  // memecah string angka menjadi array karakter / digit-digit
+  let urut = numPisah.map((pisah) =>
+    pisah
+      .split(``)
+      // map memproses numPisah dan menjadi array baru
 
-    // mengurutkan secara ascending
-    .sort()
+      // mengurutkan secara ascending
+      .sort()
 
-    // menggabungkan array karakter menjadi array of string
-    .join(``))
+      // menggabungkan array karakter menjadi array of string
+      .join(``)
+  );
 
-    // menggabungkan array menjadi satu string tanpa pemisah (``)
-    let gabungkan = urut.join(``)
+  // menggabungkan array menjadi satu string tanpa pemisah (``)
+  let gabungkan = urut.join(``);
 
-    // mengubah string angkan menjadi number
-    return Number(gabungkan);
+  // mengubah string angkan menjadi number
+  return Number(gabungkan);
 }
 // console.log(devideAndSort(5956560159466056));
