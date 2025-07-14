317ccb049fc0eacc75d0542366b8d0b927452682
federus rudi <federusrudi@gmail.com>
2025-07-14 10:24:12 +0700

    feat: menambahkan file index.js, fiturnya memanggil fungsi dan mengoutputkan  dari setiap file yang di import

---
+++ index.js
@@ -0,0 +1,24 @@
+// // NOMOR 1
+// // THEN CATCH
+// import { fetchData } from "./tugas/task-1.js";
+// fetchData(true)
+// //  ASYNC AWAIT
+// import getData from "./tugas/task-1.js";
+// getData(true)
+
+// // NOMOR 2
+// import { getDataFromServer } from "./tugas/task-2.js";
+// import { processData } from "./tugas/task-2.js";
+// getDataFromServer(true, processData);
+
+// // NOMOR 3
+// import { RestApi } from "./tugas/task-3.js";
+// RestApi()
+
+// // NOMOR 4
+// import { devideAndSort } from "./tugas/task-4.js";
+// console.log(devideAndSort(5956560159466056));
+
+// NOMOR 5
+import { tampilkanMenu } from "./tugas/task-5.js";
+tampilkanMenu()
